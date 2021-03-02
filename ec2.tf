d417a78f84fd97784d238532dc02e57af646423b
makhil1 <makhil@hotmail.com>
2021-03-02 19:24:04 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "Qlik-QA" {
+  ami           = "ami-05c6b990b0e19b937"
+  instance_type = "t2.micro"
+  availability_zone = "eu-west-2a"
+  key_name = "terraform-key"
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
+  tags = {
+    Name = "Qlik-Replcate-Active"
+  }
